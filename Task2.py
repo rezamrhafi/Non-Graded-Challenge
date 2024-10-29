2087d9a58186e053b14c4be9ea35abca326767dd
rezamrhafi <rezamrhafi@gmail.com>
2024-10-29 16:01:53 +0700

    Non challange 1

---
+++ Task2.py
@@ -0,0 +1,7 @@
+Data = [1, 4, 9, 16, 25, 36, 49,  64, 81, 100]
+
+print(Data[3])
+
+print(Data[5:-1])
+
+print(Data[::-1])
---
+++ Task3.py
@@ -0,0 +1,13 @@
+provinsi = {'Nanggroe Aceh Darussalam': 'Aceh',
+            'Sumatera Selatan': 'Palembang',
+            'Kalimantan Barat': 'Pontianak','Jawa Timur': 'Madiun',
+            'Sulawesi Selatan': 'Makassar','Maluku': 'Ambon'}
+
+
+print(provinsi.keys())
+print()
+
+provinsi['Jawa Timur'] = 'Surabaya'
+
+print(provinsi)
+
